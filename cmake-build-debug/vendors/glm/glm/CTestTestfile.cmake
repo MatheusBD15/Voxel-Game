ccebebbcab0f5a7f40e0bab3c293b3e197b9dafc
MatheusBD15 <matheusbd15@gmail.com>
2021-11-27 01:58:29 -0300

    Remove files and add to gitignore

--- cmake-build-debug/vendors/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/MatheusBD/Desktop/minecraftClone/vendors/glm/glm
-# Build directory: C:/Users/MatheusBD/Desktop/minecraftClone/cmake-build-debug/vendors/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
