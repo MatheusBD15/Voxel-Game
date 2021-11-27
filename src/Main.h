6b37bb91e0fd4ce5361e7c57fc63d90cc9c51f2c
MatheusBD15 <matheusbd15@gmail.com>
2021-11-27 14:55:15 -0300

    rename to voxelGame

--- src/Main.h
+++ src/Main.h
@@ -2,12 +2,12 @@
 // Created by MatheusBD on 26/11/2021.
 //
 
-#ifndef MINECRAFTCLONE_MAIN_H
-#define MINECRAFTCLONE_MAIN_H
+#ifndef VOXELGAME_MAIN_H
+#define VOXELGAME_MAIN_H
 
 #include <GLFW/glfw3.h>
 #include "stdint.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-#endif //MINECRAFTCLONE_MAIN_H
+#endif //VOXELGAME_MAIN_H
--- src/Window/Window.h
+++ src/Window/Window.h
@@ -2,8 +2,8 @@
 // Created by MatheusBD on 27/11/2021.
 //
 
-#ifndef MINECRAFTCLONE_WINDOW_H
-#define MINECRAFTCLONE_WINDOW_H
+#ifndef VOXELGAME_WINDOW_H
+#define VOXELGAME_WINDOW_H
 
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
@@ -24,7 +24,7 @@ public:
 
     GLFWwindow* getWindow() { return m_window; };
 
-    void processInput(GLFWwindow *window);
+    static void processInput(GLFWwindow *window);
 };
 
-#endif //MINECRAFTCLONE_WINDOW_H
+#endif //VOXELGAME_WINDOW_H
